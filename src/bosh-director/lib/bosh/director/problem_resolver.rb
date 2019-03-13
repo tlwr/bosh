0b27f6391a80cd22aaf1d880d1b10d8ea8204074
Beyhan Veli <beyhan.veli@sap.com>
2019-03-13 15:00:04 +0100

    Spike: parallel resurrection respects max_in_flight
    In this change, parallel resurrection respects the `max_in_flight` update
    property.
    
    [#163648935](https://www.pivotaltracker.com/story/show/163648935)
    
    Co-authored-by: David Ansari <david.ansari@sap.com>

--- src/bosh-director/lib/bosh/director/problem_resolver.rb
+++ src/bosh-director/lib/bosh/director/problem_resolver.rb
@@ -1,6 +1,9 @@
+require_relative 'jobs/update_deployment'
+
 module Bosh::Director
   class ProblemResolver
-    include LegacyDeploymentHelper
+    include DeploymentPlan
+    include Jobs
 
     attr_reader :logger
 
@@ -8,6 +11,12 @@ module Bosh::Director
       @deployment = deployment
       @resolved_count = 0
       @resolution_error_logs = StringIO.new
+      update_deployment = UpdateDeployment.new(
+        @deployment.manifest,
+        @deployment.cloud_configs.map(&:id),
+        @deployment.runtime_configs.map(&:id),
+      )
+      @instance_groups = update_deployment.deployment_plan.instance_groups
 
       #temp
       @event_log_stage = nil
@@ -35,8 +44,9 @@ module Bosh::Director
       if Config.parallel_problem_resolution
         # TODO: here we just assume :type recreate VM,
         # make sure that other problem types like re-attach disk still work
-        parallelizable_problems_for_each(problems.all) do |probs|
-          ThreadPool.new(max_threads: Config.max_threads).wrap do |pool|
+        problems_ordered_by_job(problems.all) do |probs, max_in_flight|
+          number_of_threads = [probs.size, max_in_flight].min
+          ThreadPool.new(max_threads: number_of_threads).wrap do |pool|
             probs.each do |problem|
               pool.process do
                 process_problem(problem)
@@ -69,26 +79,38 @@ module Bosh::Director
       end
     end
 
-    def parallelizable_problems_for_each(problems)
-      partition_jobs_by_serial(deployment_plan.instance_groups).each do |jp|
+    def problems_ordered_by_job(problems, &block)
+      BatchMultiInstanceGroupUpdater.partition_jobs_by_serial(@instance_groups).each do |jp|
         if jp.first.update.serial?
           # all instance groups in this partition are serial
           jp.each do |ig|
-            probs = select_problems_by_instance_group(problems, ig)
-            # within an instance_group parallelize recreation of all instances
-            yield probs
+            process_ig(ig, problems, block)
           end
         else
           # all instance groups in this partition are non-serial
           # therefore, parallelize recreation of all instances in this partition
-          probs = []
-          jp.each do |ig|
-            probs_for_ig = select_problems_by_instance_group(problems, ig)
-            probs << probs_for_ig
+          # therefore, create an outer ThreadPool as ParallelMultiInstanceGroupUpdater does it in the regular deploy flow
+
+          # TODO: might not want to create a new thread for instance_groups without problems
+          ThreadPool.new(max_threads: jp.size).wrap do |pool|
+            jp.each do |ig|
+              pool.process do
+                process_ig(ig, problems, block)
+              end
+            end
           end
-          yield probs.flatten
         end
       end
+    end
+
+    def process_ig(ig, problems, block)
+      instance_group = @instance_groups.find do |plan_ig|
+        plan_ig.name == ig.name
+      end
+      probs = select_problems_by_instance_group(problems, ig)
+      max_in_flight = instance_group.update.max_in_flight(probs.size)
+      # within an instance_group parallelize recreation of all instances
+      block.call(probs, max_in_flight)
     end
 
     def select_problems_by_instance_group(problems, instance_group)
@@ -99,84 +121,6 @@ module Bosh::Director
         instance.job == instance_group.name
       end
     end
-
-    # TODO this method is copied from src/bosh-director/lib/bosh/director/deployment_plan/multi_instance_group_updater.rb
-    # reuse code instead
-    def partition_jobs_by_serial(jobs)
-      job_partitions = []
-      last_partition = []
-
-      jobs.each do |j|
-        lastj = last_partition.last
-        if !lastj || lastj.update.serial? == j.update.serial?
-          last_partition << j
-        else
-          job_partitions << last_partition
-          last_partition = [j]
-        end
-      end
-
-      job_partitions << last_partition if last_partition.any?
-      job_partitions
-    end
-
-    # TODO >>>>>> below methods are copied from src/bosh-director/lib/bosh/director/jobs/update_deployment.rb
-    # resuse code instead
-    def deployment_plan
-      return @deployment_plan if @deployment_plan
-
-      deployment_manifest = Manifest.load_from_hash(manifest_hash, @deployment.manifest, cloud_config_models, runtime_config_models)
-      planner_factory = DeploymentPlan::PlannerFactory.create(logger)
-
-      @deployment_plan = planner_factory.create_from_manifest(
-        deployment_manifest,
-        cloud_config_models,
-        runtime_config_models,
-        {}, # @options,
-      )
-    end
-
-    def cloud_config_models
-      return @cloud_config_models if @cloud_config_models
-
-      if ignore_cloud_config?(manifest_hash)
-        warning = "Ignoring cloud config. Manifest contains 'networks' section."
-        logger.debug(warning)
-        # @event_log.warn_deprecated(warning)
-        @cloud_config_models = nil
-      else
-        @cloud_config_models = Bosh::Director::Models::Config.find_by_ids(@deployment.cloud_configs.map(&:id))
-        if cloud_config_models.empty?
-          logger.debug('No cloud config uploaded yet.')
-        else
-          logger.debug("Cloud config:\n#{Bosh::Director::CloudConfig::CloudConfigsConsolidator.new(cloud_config_models).raw_manifest}")
-        end
-      end
-
-      @cloud_config_models
-    end
-
-    def runtime_config_models
-      return @runtime_config_models if @runtime_config_models
-
-      @runtime_config_models = Bosh::Director::Models::Config.find_by_ids(@deployment.runtime_configs.map(&:id))
-      if runtime_config_models.empty?
-        logger.debug("No runtime config uploaded yet.")
-      else
-        logger.debug("Runtime configs:\n#{Bosh::Director::RuntimeConfig::RuntimeConfigsConsolidator.new(runtime_config_models).raw_manifest}")
-      end
-      @runtime_config_models
-    end
-
-    def manifest_hash
-      return @manifest_hash if @manifest_hash
-
-      logger.info('Reading deployment manifest')
-      @manifest_hash = YAML.load(@deployment.manifest)
-      logger.debug("Manifest:\n#{@deployment.manifest}")
-      @manifest_hash
-    end
-    # <<<<<<<< copied up until here
 
     def apply_resolution(problem)
       handler = ProblemHandlers::Base.create_from_model(problem)
