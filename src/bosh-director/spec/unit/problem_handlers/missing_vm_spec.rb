e9885b84a43904fa0092dbbeb4032288dc2a31ea
Sebastian Heid <sebastian.heid@sap.com>
2019-05-24 15:53:42 +0200

    Respect serial and max_in_flight property
    Before this change, the resolution of detected problems by a scan and fix
    task was scheduled in parallel across all instance groups in a deployment.
    After this change, `max_in_flight` and `serial` are considered.
    
    [#165062729](https://www.pivotaltracker.com/story/show/165062729)
    
    Co-authored-by: David Ansari <david.ansari@sap.com>
    Co-authored-by: Ulrich Kramer <u.kramer@sap.com>
    Co-authored-by: Vladimir Videlov <vladimir.videlov@sap.com>

--- src/bosh-director/spec/unit/problem_handlers/missing_vm_spec.rb
+++ src/bosh-director/spec/unit/problem_handlers/missing_vm_spec.rb
@@ -67,6 +67,10 @@ module Bosh::Director
       expect(handler).to be_kind_of(described_class)
     end
 
+    it 'is an instance problem' do
+      expect(handler.instance_problem?).to be_truthy
+    end
+
     it 'should call recreate_vm_skip_post_start when set to auto' do
       allow(handler).to receive(:recreate_vm_skip_post_start)
       expect(handler).to receive(:recreate_vm_skip_post_start).with(instance)
