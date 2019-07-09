5962922965057cac555e12c663eed27a88d88e31
Miguel Verissimo <mverissimo@pivotal.io>
2019-07-09 12:29:36 -0400

    WIP

--- packages/bundler/generate_blob.sh
+++ packages/bundler/generate_blob.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-gem fetch bundler --platform=x86_64-linux
-bosh add-blob ./bundler*.gem bundler/bundler.gem
+apt-get install -y git
+
+# gem fetch bundler --platform=x86_64-linux
+# bosh add-blob ./bundler*.gem bundler/bundler.gem
+
+gem install ronn:'~> 0.7.3'
+mkdir -p /tmp/bundler
+pushd /tmp/bundler
+  git clone https://github.com/bundler/bundler.git
+  pushd bundler
+    rake build
+    # gem install pkg/bundler-*.gem -i /var/vcap/packages/bundler/gems
+    bosh add-blob pkg/bundler*.gem bundler/bundler.gem
+  popd
+popd
