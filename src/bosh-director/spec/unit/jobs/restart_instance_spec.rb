dbc26c6af84fd37b1c09897d70610185e6871444
Miguel Verissimo <mverissimo@pivotal.io>
2019-07-16 12:10:49 -0400

    WIP make test setup a little bit better
    [#167149815](https://www.pivotaltracker.com/story/show/167149815)

--- src/bosh-director/spec/unit/jobs/restart_instance_spec.rb
+++ src/bosh-director/spec/unit/jobs/restart_instance_spec.rb
@@ -7,42 +7,77 @@ module Bosh::Director
 
     let(:manifest) { Bosh::Spec::NewDeployments.simple_manifest_with_instance_groups }
     let(:deployment) { Models::Deployment.make(name: 'simple', manifest: YAML.dump(manifest)) }
-    let(:instance) do
-      Models::Instance.make(
-        deployment: deployment,
-        job: 'foobar',
-        uuid: 'test-uuid',
-        index: '1',
-        spec_json: instance_spec.to_json,
-      )
-    end
-    let(:vm_model) { Models::Vm.make(instance: instance, active: true, cid: 'test-vm-cid') }
+    let(:vm_model) { Models::Vm.make(instance: instance_model, active: true, cid: 'test-vm-cid') }
     let(:task) { Models::Task.make(id: 42) }
     let(:task_writer) { TaskDBWriter.new(:event_output, task.id) }
     let(:event_log) { EventLog::Log.new(task_writer) }
     let(:cloud_config) { Models::Config.make(:cloud_with_manifest_v2) }
     let(:variables_interpolator) { ConfigServer::VariablesInterpolator.new }
+    let(:event_log_stage) { instance_double('Bosh::Director::EventLog::Stage') }
+    let(:vm_creator) { instance_double(VmCreator, create_for_instance_plan: nil) }
+    let(:template_persister) { instance_double(RenderedTemplatesPersister, persist: nil) }
+    let(:blobstore) { instance_double(Bosh::Blobstore::Client) }
+    let(:local_dns_manager) { instance_double(LocalDnsManager, update_dns_record_for_instance: nil) }
+    let(:state_applier) { instance_double(InstanceUpdater::StateApplier, apply: nil) }
     let(:unmount_instance_disk_step) { instance_double(DeploymentPlan::Steps::UnmountInstanceDisksStep, perform: nil) }
     let(:delete_vm_step) { instance_double(DeploymentPlan::Steps::DeleteVmStep, perform: nil) }
-    let(:event_log_stage) { instance_double('Bosh::Director::EventLog::Stage') }
-    let(:agent_client) { instance_double(AgentClient, run_script: nil, drain: 0, stop: nil) }
     let!(:stemcell) { Bosh::Director::Models::Stemcell.make(name: 'stemcell-name', version: '3.0.2', cid: 'sc-302') }
-    let(:deployment_plan_instance) do
-      instance_double(DeploymentPlan::Instance,
-        template_hashes: nil,
-        rendered_templates_archive: nil,
-        configuration_hash: nil)
+
+    let!(:instance_spec) do
+      {
+        'stemcell' => {
+          'name' => stemcell.name,
+          'version' => stemcell.version,
+        },
+        'env' => { 'key1' => 'value1' },
+      }
     end
 
-    let(:instance_spec) do
+    let(:instance_model) do
+      Models::Instance.make(
+        deployment: deployment,
+        job: 'foobar',
+        uuid: 'test-uuid',
+        index: '1',
+        state: 'started',
+        spec_json: instance_spec.to_json,
+      )
+    end
+
+    let(:agent_client) do
+      instance_double(
+        AgentClient,
+        run_script: nil,
+        start: nil,
+        apply: nil,
+        get_state: { 'job_state' => 'running' },
+      )
+    end
+
+    describe 'DelayedJob job class expectations' do
+      let(:job_type) { :start_instance }
+      let(:queue) { :normal }
+      it_behaves_like 'a DJ job'
+    end
+
+    let(:deployment_plan_instance) do
+      instance_double(
+        DeploymentPlan::Instance,
+        template_hashes: nil,
+        rendered_templates_archive: nil,
+        configuration_hash: nil,
+      )
+    end
+
+    let(:spec) do
       {
         'vm_type' => {
           'name' => 'vm-type-name',
           'cloud_properties' => {},
         },
         'stemcell' => {
-          'name' => stemcell.name,
-          'version' => stemcell.version,
+          'name' => 'stemcell-name',
+          'version' => '2.0.6',
         },
         'networks' => {},
       }
@@ -55,7 +90,7 @@ module Bosh::Director
       release_version = Models::ReleaseVersion.make(version: '0.1-dev', release: release)
       template1 = Models::Template.make(name: 'foobar', release: release)
       release_version.add_template(template1)
-      allow(instance).to receive(:active_vm).and_return(vm_model)
+      allow(instance_model).to receive(:active_vm).and_return(vm_model)
 
       allow(Config).to receive(:event_log).and_call_original
       allow(Config.event_log).to receive(:begin_stage).and_return(event_log_stage)
@@ -74,8 +109,8 @@ module Bosh::Director
 
     describe 'perform' do
       it 'should restart the instance' do
-        job = Jobs::RestartInstance.new(deployment.name, instance.id, {})
-        expect(instance.state).to eq 'started'
+        job = Jobs::RestartInstance.new(deployment.name, instance_model.id, {})
+        expect(instance_model.state).to eq 'started'
         job.perform
 
         expect(state_applier).to have_received(:apply)
@@ -83,7 +118,7 @@ module Bosh::Director
       end
 
       it 'obtains a deployment lock' do
-        job = Jobs::RestartInstance.new(deployment.name, instance.id, {})
+        job = Jobs::RestartInstance.new(deployment.name, instance_model.id, {})
         expect(job).to receive(:with_deployment_lock).with('simple').and_yield
         job.perform
       end
@@ -91,7 +126,7 @@ module Bosh::Director
       it 'logs starting' do
         expect(Config.event_log).to receive(:begin_stage).with('restarting instance foobar').and_return(event_log_stage)
         expect(event_log_stage).to receive(:advance_and_track).with('foobar/test-uuid (1)').and_yield
-        job = Jobs::RestartInstance.new(deployment.name, instance.id, {})
+        job = Jobs::RestartInstance.new(deployment.name, instance_model.id, {})
         job.perform
       end
 
